6ddabc4ad6109b142c548db020d6da4bf853b63f
Shannon Smith <infiltratr@yandex.com>
2026-09-01 23:53:46 +1000

    Add native iPhone GUI diagnostics

---
+++ platform/apple/AudiLinkBLETransport.h
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+#import "../../src/link/platform/apple/LinkBLETransport.h"
+#define AudiLinkBLETransport LinkBLETransport
+#define AudiLinkBLETransportDelegate LinkBLETransportDelegate
+typedef LinkBLETransportState AudiLinkBLETransportState;
