b7a874d0f2e0f06248734d361c0d4dba1267ca66
Shannon Smith <infiltratr@yandex.com>
2026-09-03 09:41:20 +1000

    Add AUDILINK product icon

--- app/linux/main.c
+++ app/linux/main.c
@@ -377,6 +377,7 @@ int main(int argc, char **argv)
     descriptor.brand_name = "AUDILINK";
     descriptor.brand_subtitle = "AUDI · LINK DIAGNOSTICS";
     descriptor.version = audilink_version();
+    descriptor.emblem_resource = "/com/github/Infiltrator-Projects/AUDILINK/audilink-emblem.png";
     descriptor.css = css;
     descriptor.render_section = render;
     descriptor.connection_changed = connection_changed;
