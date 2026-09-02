1acffb1f4ce03c5583816e95e1dae93549044979
Shannon Smith <infiltratr@yandex.com>
2026-09-02 15:35:34 +1000

    Set AUDILINK public version 0.2.10

--- include/audilink/audilink.h
+++ include/audilink/audilink.h
@@ -2,7 +2,7 @@
 #ifndef AUDILINK_H
 #define AUDILINK_H
 #ifndef AUDILINK_VERSION
-#define AUDILINK_VERSION "0.2.9"
+#define AUDILINK_VERSION "0.2.10"
 #endif
 #define AUDILINK_BRAND "Audi"
 const char *audilink_product_name(void);
