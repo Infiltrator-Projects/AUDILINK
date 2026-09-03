3bbe9bba44b959acd5fa700426f781358f43c990
Shannon Smith <infiltratr@yandex.com>
2026-09-03 20:07:52 +1000

    Release 0.2.16: LINK-owned themed About surfaces

--- include/audilink/audilink.h
+++ include/audilink/audilink.h
@@ -2,7 +2,7 @@
 #ifndef AUDILINK_H
 #define AUDILINK_H
 #ifndef AUDILINK_VERSION
-#define AUDILINK_VERSION "0.2.15"
+#define AUDILINK_VERSION "0.2.16"
 #endif
 #define AUDILINK_BRAND "Audi"
 const char *audilink_product_name(void);
