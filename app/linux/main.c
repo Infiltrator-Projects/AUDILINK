95caf8a19972dc476454c9a3a4b586bee3ee1dc7
Shannon Smith <infiltratr@yandex.com>
2026-09-20 12:21:52 +1000

    Update copyright start year to 1993

--- app/linux/main.c
+++ app/linux/main.c
@@ -378,7 +378,7 @@ int main(int argc, char **argv)
         .version = audilink_version(),
         .description = "A C-first, open-source Audi vehicle diagnostics platform authored by Shannon Smith.",
         .authors = "Shannon Smith",
-        .copyright = "Copyright © 2000-2026 Shannon Smith",
+        .copyright = "Copyright © 1993-2026 Shannon Smith",
         .website = "https://github.com/Infiltrator-Projects/AUDILINK",
         .license_name = "GPL-3.0-or-later",
         .license_text =
